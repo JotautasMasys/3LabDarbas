ccaac366244c50d80b8039ab840d66df0fa39fab
JotautasMasys <mjotautas25@gmail.com>
2023-12-19 20:08:41 +0200

    sixth commit

--- app/src/androidTest/java/com/example/a3labdarbas/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/a3labdarbas/ExampleInstrumentedTest.java
@@ -5,6 +5,15 @@ import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -22,5 +31,20 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.a3labdarbas", appContext.getPackageName());
+    }
+
+    @Rule
+    public ExampleInstrumentedTest<MainActivity> activityRule = new ExampleInstrumentedTest<>(MainActivity.class);
+
+    @Test
+    public void testButtonClicks() {
+        Espresso.onView(ViewMatchers.withId(R.id.n1)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.n2)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.plus)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.n3)).perform(ViewActions.click());
+        Espresso.onView(ViewMatchers.withId(R.id.result)).perform(ViewActions.click());
+
+        Espresso.onView(ViewMatchers.withId(R.id.calculatorScreen))
+                .check(ViewAssertions.matches(ViewMatchers.withText("15")));
     }
 }
--- app/src/test/java/com/example/a3labdarbas/ExampleUnitTest.java
+++ app/src/test/java/com/example/a3labdarbas/ExampleUnitTest.java
@@ -1,8 +1,11 @@
 package com.example.a3labdarbas;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+
+import android.widget.Button;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +16,67 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    private MainActivity mainActivity;
+
+    @Before
+    public void setUp() {
+        mainActivity = new MainActivity();
+    }
+
+    @Test
+    public void testAddition() {
+        String result = mainActivity.getResult("2+3");
+        assertEquals("5", result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        String result = mainActivity.getResult("5-3");
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void testMultiplication() {
+        String result = mainActivity.getResult("2*3");
+        assertEquals("6", result);
+    }
+
+    @Test
+    public void testDivision() {
+        String result = mainActivity.getResult("6/3");
+        assertEquals("2", result);
+    }
+
+    @Test
+    public void testComplexExpression() {
+        String result = mainActivity.getResult("2+3*4-2");
+        assertEquals("12", result);
+    }
+
+    @Test
+    public void testInvalidExpression() {
+        String result = mainActivity.getResult("2+*3");
+        assertEquals("ERROR", result);
+    }
+
+    @Test
+    public void testClearEverything() {
+        mainActivity.onClick(mockButtonWithText("CE"));
+        assertEquals("", mainActivity.calculatorScreen.getText().toString());
+    }
+
+    @Test
+    public void testDeleteLastCharacter() {
+        mainActivity.calculatorScreen.setText("123");
+        mainActivity.onClick(mockButtonWithText("DEL"));
+        assertEquals("12", mainActivity.calculatorScreen.getText().toString());
+    }
+
+    private Button mockButtonWithText(String text) {
+        Button button = new Button(mainActivity);
+        button.setText(text);
+        return button;
     }
 }
